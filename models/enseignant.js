dda223dceb243e90c9f52f30fd85cab0325f53ac
Momo23213 <soumaho535@gmail.com>
2025-09-12 10:40:34 +0000

    bien

--- models/enseignant.js
+++ models/enseignant.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const enseignantSchema = new mongoose.Schema({
   nom: { type: String, required: true },
   prenom: { type: String, required: true },
+  telephone: { type: String },
   email: { type: String, required: true, unique: true },
   matieres: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Matiere' }],
+  classe: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Classe' }],
   photo: { type: String },
-  createdAt: { type: Date, default: Date.now }
+  dateEmbauche: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Enseignant', enseignantSchema);
