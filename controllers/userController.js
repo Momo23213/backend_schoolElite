e4376db0b38d852e7b022d57c96aa523d545bfa1
Momo23213 <soumaho535@gmail.com>
2025-09-18 21:02:54 +0000

    oddl

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,20 +22,52 @@ exports.getUserById = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
-
-// CREATE
 exports.createUser = async (req, res) => {
   try {
-    const data = req.body;
-    if (req.file) data.photo = `/uploads/${req.file.filename}`;
+    const { pseudo,email, password, role } = req.body;
 
-    const newUser = new User(data);
+    // Vérifier si l'utilisateur existe déjà
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: "Email déjà utilisé" });
+
+    // Hasher le mot de passe
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      pseudo,
+      email,
+      password: hashedPassword,
+      role
+    });
+
     const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
+
+    res.status(201).json({
+      message: "Utilisateur créé avec succès",
+      user: {
+        id: savedUser._id,
+        pseudo: savedUser.pseudo,
+        email: savedUser.email,
+        role: savedUser.role
+      }
+    });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
+// CREATE
+// exports.createUser = async (req, res) => {
+//   try {
+//     const data = req.body;
+//     if (req.file) data.photo = `/uploads/${req.file.filename}`;
+
+//     const newUser = new User(data);
+//     const savedUser = await newUser.save();
+//     res.status(201).json(savedUser);
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// };
 
 // UPDATE
 exports.updateUser = async (req, res) => {
